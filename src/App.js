8feadb96bdcbf976c948ab28d07a2a0f766dc195
rud2403 <qkrrudals2403@naver.com>
2021-01-08 17:51:42 +0900

    리액트 자바스크립트_커스터머 작업

--- src/App.js
+++ src/App.js
@@ -2,22 +2,53 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
   'name' : '홍길동',
   'birthday' : '961222',
   'gender' : '남자',
   'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '김철수',
+  'birthday' : '951213',
+  'gender' : '남자',
+  'job' : '프로그래머'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '홍길순',
+  'birthday' : '920326',
+  'gender' : '여자',
+  'job' : '디자이너'
 }
+]
 
   class App extends Component {
     render() {
       return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+        <div>
+          {
+          customers.map(c => {
+            return (
+             <Customer 
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.name}
+              gender={c.name}
+              job={c.job}
+            />
+            );
+          })
+          }
+        </div>
     );
   }
 }
